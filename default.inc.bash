79a11e8b6517dc27d3940b3fde56803b3daa8a96
Mattijs Snepvangers <pegasus.ict@gmail.com>
2018-05-17 11:35:47 +0200

    .

--- default.inc.bash
+++ default.inc.bash
@@ -18,6 +18,12 @@
 #########################################################
 
 unset CDPATH # to prevent mishaps when using cd with relative paths
+# A better class of script...
+set -o errexit		# Exit on most errors (see the manual)
+set -o errtrace		# Make sure any error trap is inherited
+set -o nounset		# Disallow expansion of unset variables
+set -o pipefail		# Use last non-zero exit code in a pipeline
+set -o xtrace		# Trace the execution of the script (debug)
 
 ### FUNCTIONS ###
 create_constants() { ### defines constants

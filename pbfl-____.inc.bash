f9b805b243fff84e6ebfa8546c3ef6772258fad5
Mattijs Snepvangers <pegasus.ict@gmail.com>
2018-06-10 08:06:10 +0200

    improved the library template
    .
    
    [Ticket: X]
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch master
    # Changes to be committed:
    #   (use "git reset HEAD <file>..." to unstage)
    #
    # modified:   lib/test.rb
    #

--- pbfl-____.inc.bash
+++ pbfl-____.inc.bash
@@ -1,19 +1,24 @@
-#!/bin/bash -p
+#!/bin/bash
 ############################################################################
-# Pegasus' Linux Administration Tools #		Pegasus' Bash Function Library #
-# (C)2017-2018 Mattijs Snepvangers	  #				 pegasus.ict@gmail.com #
-# License: MIT						  #	Please keep my name in the credits #
+# Pegasus' Linux Administration Tools #					<<script title>> #
+# (C)2017-<<year>> Mattijs Snepvangers	  #				 pegasus.ict@gmail.com #
+# License: MIT						  # Please keep my name in the credits #
 ############################################################################
+
+# !!! first replace (ctrl-h)
+# <<year>> with the current year
+# "<<date>>" with todays date
+# <<script title>> with the title of the script and adjust the number of tabs if needed for proper alignment
 
 #######################################################
 # PROGRAM_SUITE="Pegasus' Linux Administration Tools" #
-# SCRIPT_TITLE="Sed Functions Script"				  #
+# SCRIPT_TITLE="Sscript title>>"				  #
 # MAINTAINER="Mattijs Snepvangers"					  #
 # MAINTAINER_EMAIL="pegasus.ict@gmail.com"			  #
 # VERSION_MAJOR=0									  #
 # VERSION_MINOR=0									  #
 # VERSION_PATCH=0									  #
 # VERSION_STATE="PRE-ALPHA"							  #
-# VERSION_BUILD=20180531							  #
+# VERSION_BUILD="<<date>>"							  #
 # LICENSE="MIT"										  #
 #######################################################
